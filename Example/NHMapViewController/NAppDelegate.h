2873366fb5018a9bd265a1c82a5933a5af704886
Naithar <devias.naith@gmail.com>
2015-05-28 15:27:26 +0300

    Initial commit

---
+++ Example/NHMapViewController/NAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NAppDelegate.h
+//  NHMapViewController
+//
+//  Created by CocoaPods on 05/28/2015.
+//  Copyright (c) 2014 Naithar. All rights reserved.
+//
+
+@import UIKit;
+
+@interface NAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/NHMapViewController/NViewController.h
@@ -0,0 +1,13 @@
+//
+//  NViewController.h
+//  NHMapViewController
+//
+//  Created by Naithar on 05/28/2015.
+//  Copyright (c) 2014 Naithar. All rights reserved.
+//
+
+@import UIKit;
+
+@interface NViewController : UIViewController
+
+@end
