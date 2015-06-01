16d77d785800d458b996b547171ee1e7ca4f73f6
Naithar <devias.naith@gmail.com>
2015-06-01 17:35:36 +0300

    removed unneeded init

--- Pod/Classes/NHMapNavigationViewController.m
+++ Pod/Classes/NHMapNavigationViewController.m
@@ -17,15 +17,15 @@
 
 @implementation NHMapNavigationViewController
 
-- (instancetype)init {
-    self = [super init];
-    
-    if (self) {
-        [self commonInit];
-    }
-    
-    return self;
-}
+//- (instancetype)init {
+//    self = [super init];
+//    
+//    if (self) {
+//        [self commonInit];
+//    }
+//    
+//    return self;
+//}
 
 - (id)initWithCoder:(NSCoder *)aDecoder {
     self = [super initWithCoder:aDecoder];
