43a70b56941f78788e4d46b63f66b05625432290
Naithar <devias.naith@gmail.com>
2015-06-03 13:40:35 +0300

    annotation offset change

--- Pod/Classes/NHMapViewAnnotation.m
+++ Pod/Classes/NHMapViewAnnotation.m
@@ -28,7 +28,7 @@
 - (void)commonInit {
     self.annotationView = [[NHMapViewAnnotationView alloc] init];
     self.annotationView.image = [UIImage imageNamed:@"NHMapView.marker.png"];
-    self.annotationView.centerOffset = CGPointMake(0, -5);
+    self.annotationView.centerOffset = CGPointMake(0, -13);
     self.annotationView.canShowCallout = YES;
 }
 
